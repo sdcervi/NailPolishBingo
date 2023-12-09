5371973430d96ef9e02f066191028ac82f63bcd5
sdcervi <sdcervi@gmail.com>
2023-12-09 09:35:29 -0800

    Starting to build

---
+++ js/nav.js
@@ -0,0 +1,52 @@
+// Set up primary nav contents
+const primaryNav = document.getElementById('primaryNav');
+
+let primaryNavContents = '<div class="container-fluid">';
+primaryNavContents += '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>';
+primaryNavContents += '<a class="navbar-brand" href="/">Nail Polish <img src="/assets/logo.svg" alt="" /> Bingo<span class="d-none d-lg-inline">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</span></a>';
+primaryNavContents += '<div class="collapse navbar-collapse" id="navbarCollapse">';
+primaryNavContents += '<ul class="navbar-nav mr-auto">';
+primaryNavContents += '<li class="nav-item" id="nav-home"><a class="nav-link" href="/">Home</a></li>';
+primaryNavContents += '<li class="nav-item" id="nav-stash"><a class="nav-link" href="/stash/">Stash</a></li>';
+primaryNavContents += '<li class="nav-item" id="nav-library"><a class="nav-link" href="/library/">Library</a></li>';
+primaryNavContents += '<li class="nav-item" id="nav-about"><a class="nav-link" href="/about/">About</a></li>';
+primaryNavContents += '<li class="nav-item" id="nav-faq"><a class="nav-link" href="/faq/">FAQ</a></li>';
+primaryNavContents += '<li class="nav-item" id="nav-feedback"><a class="nav-link" href="/feedback/">Feedback</a></li>';
+primaryNavContents += '<li class="nav-item" id="nav-news"><a class="nav-link" href="/news/">News</a></li>';
+primaryNavContents += '<li class="nav-item" id="nav-donate"><a class="nav-link" href="/donate/">Donate</a></li>';
+primaryNavContents += '</ul></div>';
+	
+primaryNavContents += '<div class="data-loading" id="data-loading"><div class="spinner-border text-primary" role="status"></div><span class="loading-text">&nbsp;Loading</span></div>';
+primaryNavContents += '<div class="dropdown" id="profileDropdown"><button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-label="Toggle profile drop-down"><img src="/assets/icons/profile.svg"><span id="profileUserNavbar"></span></button>';
+primaryNavContents += '<ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">';
+primaryNavContents += '<li><a class="dropdown-item" href="/profile/">Profile &amp; Settings</a></li>';
+primaryNavContents += '<li><hr class="dropdown-divider"></li>';
+primaryNavContents += '<li><a class="dropdown-item" onclick="logout()" href="#">Sign out</a></li>';
+primaryNavContents += '</ul></div>';
+primaryNavContents += '<a href="/signin/" id="signin-button"><button class="btn btn-primary btn-sm signin" type="button">Sign in</button></a>';
+primaryNavContents += '</div>';
+primaryNavContents += '</div>';
+
+
+primaryNav.innerHTML = primaryNavContents;
+
+// Set up footer contents
+const footer = document.getElementById('footer');
+let footerContents = '<div class="container">';
+footerContents += '<p class="small text-center">Site design and layout by <a href="https://stephaniecervi.design" target="_blank">Stephanie Cervi</a>. <a href="/privacy-policy.html">Privacy Policy</a> - <a href="/terms-of-service.html">Terms of Service</a> - <a href="/feedback/">Contact us</a><br>Nail Polish Bingo is a fan-run project, and we are not sponsored by or affiliated with any of the brands featured. All products named are owned by their respective brands. All rights belong to the image holder&nbsp;credited.</p></div>';
+
+footer.innerHTML = footerContents;
+
+// Get the active page and set CSS class
+const currentPage = document.location.pathname;
+
+let filename = currentPage.slice(1, -1);
+if (!filename) {
+	filename = 'home';
+}
+let navID = `nav-${filename}`;
+
+const navElement = document.getElementById(navID);
+if (navElement) {
+	navElement.classList.add('active');
+}
---
+++ js/toggleState.js
@@ -0,0 +1,451 @@
+// Create event listeners for user clicking on anything in the webpage
+// Thanks to event delegation, this bubbles up with the unique element each time
+if (document.addEventListener) {
+    document.addEventListener("click", handleClick, false);
+    document.addEventListener("contextmenu", handleRightClick, false);
+    document.addEventListener("long-press", handleLongPress, false);
+}
+let supportsContextMenu = false;
+
+// Checks if all Pokemon in a card are caught
+function checkComplete (pokemonID, autoCollapse) {
+	const box = document.getElementById(pokemonID).parentNode.parentNode.parentNode.parentNode;
+	const listPokemon = box.children[1].firstChild.children;
+	let listComplete = [];
+	for (pokemon of listPokemon) {
+		listComplete.push(/caught/.test(pokemon.firstChild.className) || /lv100/.test(pokemon.firstChild.className));
+	}
+	if (listComplete.every(value => value === true)) {
+		box.classList.add('completed');
+	} else {
+		box.classList.remove('completed');
+	}
+	
+	// If user has auto-collapse enabled
+	if (autoCollapse) {
+		autoCollapseComplete();
+	}
+}
+
+function toggleCaughtState (pokemon, view) {
+	let toggleState = document.getElementById(pokemon).classList;
+	let endState;
+	
+	if (view === 'grid') {
+		switch (toggleState.item(2)) {
+			case null:
+				toggleState.add("caught");
+				endState = "caught";
+				checkComplete (pokemon);
+				break;
+			case "trade":
+				toggleState.remove("trade");
+				toggleState.add("caught");
+				endState = "caught";
+				checkComplete (pokemon);
+				break;
+			case "place":
+				toggleState.remove("place");
+				toggleState.add("caught");
+				endState = "caught";
+				checkComplete (pokemon);
+				break;
+			case "lv100":
+				toggleState.remove("lv100");
+				toggleState.remove("caught");
+				endState = "";
+				checkComplete (pokemon);
+				break;
+			case "caught":
+				toggleState.remove("caught");
+				endState = "";
+				checkComplete (pokemon);
+				break;
+		}
+	} else if (view === 'list') {
+		switch (toggleState.item(0)) {
+			case null:
+				toggleState.add("caught");
+				endState = "caught";
+				break;
+			case "trade":
+				toggleState.remove("trade");
+				toggleState.add("caught");
+				endState = "caught";
+				break;
+			case "place":
+				toggleState.remove("place");
+				toggleState.add("caught");
+				endState = "caught";
+				break;
+			case "lv100":
+				toggleState.remove("lv100");
+				toggleState.remove("caught");
+				endState = "";
+				break;
+			case "caught":
+				toggleState.remove("caught");
+				endState = "";
+				break;
+		}
+	}
+
+	const user = firebase.auth().currentUser;
+	if (user) {
+		const userData = db.collection('userData').doc(user.uid);
+		userData.set({
+			pokemon: {
+				[pokemon]: endState
+			}
+		}, { merge: true }).then(() => {
+			if (view === 'grid') {
+				checkComplete (pokemon);
+			}
+		}).catch ((error) => {
+			console.error ('Error updating user data: ', error);
+		});
+	} else {
+		if (view === 'grid') {
+			checkComplete (pokemon);
+		}
+	}
+}
+
+function setStateModalOpen (pokemon, view) {
+	const sourcePokemon = document.getElementById(pokemon);
+	let sourceImage;
+	let sourceNumber;
+	let sourceName;
+	
+	if (view === 'grid') {
+		sourceImage = sourcePokemon.firstChild.childNodes[0].src;
+		sourceNumber = sourcePokemon.firstChild.childNodes[1].innerHTML;
+		sourceName = sourcePokemon.firstChild.childNodes[2].innerHTML;
+	} else if (view === 'list') {
+		sourceImage = sourcePokemon.firstChild.src;
+		sourceNumber = sourcePokemon.parentNode.parentNode.childNodes[0].innerHTML;
+		sourceName = sourcePokemon.parentNode.parentNode.childNodes[1].innerHTML;
+	}
+	
+	document.getElementById('setStateTradeImage').src = sourceImage;
+	document.getElementById('setStateTradeNumber').innerHTML = sourceNumber;
+	document.getElementById('setStateTradeName').innerHTML = sourceName;
+	document.getElementById('setStatePlaceImage').src = sourceImage;
+	document.getElementById('setStatePlaceNumber').innerHTML = sourceNumber;
+	document.getElementById('setStatePlaceName').innerHTML = sourceName;
+	document.getElementById('setStateLv100Image').src = sourceImage;
+	document.getElementById('setStateLv100Number').innerHTML = sourceNumber;
+	document.getElementById('setStateLv100Name').innerHTML = sourceName;
+	
+	document.getElementById('setStateMenu').setAttribute('pokemonid', pokemon);
+	document.getElementById('setStateMenu').setAttribute('viewtype', view);
+	
+	let setStateModal = new bootstrap.Modal(document.getElementById('setStateMenu'), {});
+	setStateModal.show();
+	
+	return false;
+}
+
+function setState (state) {
+	const endState = state.substring(state.length - 5).toLowerCase();
+	const pokemon = document.getElementById('setStateMenu').getAttribute('pokemonid');
+	const view = document.getElementById('setStateMenu').getAttribute('viewtype');
+	
+	const pokemonDiv = document.getElementById(pokemon);
+	
+	// If the user clicks on the same new state as current state
+	if (pokemonDiv.classList.contains("trade") && endState === "trade") {
+		pokemonDiv.classList.remove("trade");
+	} else if (pokemonDiv.classList.contains("place") && endState === "place") {
+		pokemonDiv.classList.remove("place");
+	} else if (pokemonDiv.classList.contains("lv100") && endState === "lv100") {
+		pokemonDiv.classList.remove("lv100");
+	} else {
+		pokemonDiv.classList.remove("trade");
+		pokemonDiv.classList.remove("place");
+		pokemonDiv.classList.remove("lv100");
+		pokemonDiv.classList.remove("caught");
+		pokemonDiv.classList.add(endState);
+	}
+	
+	const user = firebase.auth().currentUser;
+	if (user) {
+		const userData = db.collection('userData').doc(user.uid);
+		userData.set({
+			pokemon: {
+				[pokemon]: endState
+			}
+		}, { merge: true }).then(() => {
+			if (view === 'grid') {
+				checkComplete (pokemon);
+			}
+		}).catch ((error) => {
+			console.error ('Error updating user data: ', error);
+		});
+	} else {
+		if (view === 'grid') {
+			checkComplete (pokemon);
+		}
+	}
+	
+	// Close modal now that we're done with it
+	document.getElementById('setStateMenuClose').click();
+}
+
+function autoCollapseComplete () {
+	const dex = document.getElementsByClassName('dex-box');
+	for (box of dex) {
+		const collapse = box.children[1];
+		const cardID = document.getElementById(collapse.id);
+		if (collapse.classList.contains('show') && box.classList.contains('completed')) {
+			const bsCollapse = new bootstrap.Collapse(cardID, {
+				show: false
+			});
+			collapse.parentNode.firstChild.firstChild.classList.add('turned');
+		}
+	}
+}
+
+function dexCollapse (toggleID) {
+	const operation = toggleID.split("-")[2];
+	const subset = toggleID.split("-")[3];
+	const dex = document.getElementById(toggleID).parentNode.parentNode.parentNode.parentNode.children[2].firstChild.children;
+	if (operation === 'expand') {
+		for (box of dex) {
+			const collapse = box.firstChild.children[1];
+			const cardID = document.getElementById(collapse.id);
+			if (!(collapse.classList.contains('show'))) {
+				const bsCollapse = new bootstrap.Collapse(cardID, {
+					show: true
+				});
+			}
+			collapse.parentNode.firstChild.firstChild.classList.remove('turned');
+		}
+	} else if (operation === 'collapse') {
+		if (subset === 'all') {
+			for (box of dex) {
+				const collapse = box.firstChild.children[1];
+				const cardID = document.getElementById(collapse.id);
+				if (collapse.classList.contains('show')) {
+					const bsCollapse = new bootstrap.Collapse(cardID, {
+						show: false
+					});
+				}
+				collapse.parentNode.firstChild.firstChild.classList.add('turned');
+			}
+		} else if (subset === 'complete') {
+			for (box of dex) {
+				const collapse = box.firstChild.children[1];
+				const cardID = document.getElementById(collapse.id);
+				if (collapse.classList.contains('show') && box.firstChild.classList.contains('completed')) {
+					const bsCollapse = new bootstrap.Collapse(cardID, {
+						show: false
+					});
+					collapse.parentNode.firstChild.firstChild.classList.add('turned');
+				}
+			}
+		} else {
+			console.log ("Error in collapse function.");
+		}
+	} else if (operation === 'showhide') {
+		const showHide = document.getElementById(toggleID);
+		const dexDiv = document.getElementById(toggleID).parentNode.parentNode.parentNode.parentNode.children[2];
+		if (showHide.firstChild.innerHTML.includes('show')) {
+			dexDiv.style.display = 'block';
+			showHide.firstChild.innerHTML = `<img src="../assets/icons/hide.svg" alt="">&nbsp;Hide&nbsp;`;
+		} else if (showHide.firstChild.innerHTML.includes('hide')) {
+			dexDiv.style.display = 'none';
+			showHide.firstChild.innerHTML = `<img src="../assets/icons/show.svg" alt="">&nbsp;Show&nbsp;`;
+		} else {
+			console.log ("Error in show/hide function");
+		}
+	} else {
+		console.log ("Error in expand-collapse function");
+	}
+}
+
+// Event handler for user clicks
+function handleClick (event) {
+    event = event || window.event;
+    event.target = event.target || event.srcElement;
+
+    let element = event.target;
+
+    // Climb up the document tree from the target of the event
+    while (element) {
+		if (element.nodeName === "IMG" && /collapse-arrow/.test(element.className)) {
+			element.classList.toggle('turned');
+			break;
+		} else if (element.nodeName === "H3" && /card-header/.test(element.parentNode.parentNode.className)) {
+			element.parentNode.parentNode.firstChild.classList.toggle('turned');
+			break;
+		} else if (element.nodeName === "DIV" && /dex-entry/.test(element.className) && !(/setState/.test(element.id))) {
+			toggleCaughtState(element.id, 'grid');
+			break;
+		} else if (element.nodeName === "IMG" && /dex-entry-img/.test(element.parentNode.parentNode.className)) {
+			toggleCaughtState(element.parentNode.id, 'list');
+			break;
+		} else if (element.nodeName === "DIV" && /dex-entry-img/.test(element.parentNode.className)) {
+			toggleCaughtState(element.id, 'list');
+			break;
+		} else if (element.nodeName === "DIV" && /dex-entry-list/.test(element.className)) {
+			toggleCaughtState(element.id);
+			break;
+		} else if (element.nodeName === "LI" && /nav-item/.test(element.className)) {
+			dexCollapse(element.id);
+			break;
+		} else if (element.nodeName === "IMG" && /setState/.test(element.id)) {
+			setState(element.parentNode.parentNode.id);
+			break;
+		} else if (element.nodeName === "DIV" && /setState/.test(element.id) && !(/setStateMenu/.test(element.id))) {
+			setState(element.id);
+			break;
+		}
+		
+        element = element.parentNode;
+    }
+}
+
+// Event handler for user clicks
+function handleRightClick (event) {
+	supportsContextMenu = true;
+	
+    event = event || window.event;
+    event.target = event.target || event.srcElement;
+
+    let element = event.target;
+	
+	// Climb up the document tree from the target of the event
+	while (element) {
+		if (element.nodeName === "DIV" && /dex-entry/.test(element.className) && !(/setState/.test(element.id))) {
+			event.preventDefault(); // Prevents normal right-click menu from showing up
+			setStateModalOpen(element.id, 'grid');
+			break;
+		} else if (element.nodeName === "IMG" && /dex-entry-img/.test(element.parentNode.parentNode.className) && !(/setState/.test(element.parentNode.parentNode.id))) {
+			event.preventDefault(); // Prevents normal right-click menu from showing up
+			setStateModalOpen(element.parentNode.id, 'list');
+			break;
+		} else if (element.nodeName === "DIV" && /dex-entry-list/.test(element.className) && !(/setState/.test(element.id))) {
+			event.preventDefault(); // Prevents normal right-click menu from showing up
+			setStateModalOpen(element.id);
+			break;
+		}
+
+		element = element.parentNode;
+	}
+	
+	return false;
+}
+
+function handleLongPress (event) {
+	event.preventDefault();
+	
+	if (!supportsContextMenu) {
+		event = event || window.event;
+		event.target = event.target || event.srcElement;
+
+		let element = event.target;
+
+		// Climb up the document tree from the target of the event
+		while (element) {
+			if (element.nodeName === "DIV" && /dex-entry/.test(element.className) && !(/setState/.test(element.id))) {
+				event.preventDefault(); // Prevents normal right-click menu from showing up
+				document.getElementById('bugnote').style.display = 'block';
+				setStateModalOpen(element.id, 'grid');
+				break;
+			} else if (element.nodeName === "IMG" && /dex-entry-img/.test(element.parentNode.parentNode.className) && !(/setState/.test(element.parentNode.parentNode.id))) {
+				event.preventDefault(); // Prevents normal right-click menu from showing up
+				document.getElementById('bugnote').style.display = 'block';
+				setStateModalOpen(element.parentNode.id, 'list');
+				break;
+			} else if (element.nodeName === "DIV" && /dex-entry-list/.test(element.className) && !(/setState/.test(element.id))) {
+				event.preventDefault(); // Prevents normal right-click menu from showing up
+				document.getElementById('bugnote').style.display = 'block';
+				setStateModalOpen(element.id);
+				break;
+			}
+
+			element = element.parentNode;
+		}
+		
+	}
+	
+	return false;
+}
+
+// Gets the already-completed items from user data and checks those boxes
+function writeUserDex (pokemonData, autoCollapse) {
+	if (Object.keys(pokemonData).length) {
+		if (listView == 'false' || listView == null)
+		{
+			for (const pokemon in pokemonData) {
+				const pokemonDiv = document.getElementById(pokemon);
+				if (pokemonData[pokemon] && pokemonDiv) {
+					pokemonDiv.classList.add(pokemonData[pokemon]);
+					checkComplete (pokemon);
+				}
+			}
+			if (autoCollapse) {
+				autoCollapseComplete();
+			}
+		} else {
+			for (const pokemon in pokemonData) {
+				if (document.getElementById(pokemon) && pokemonData[pokemon]) {
+					const pokemonDiv = document.getElementById(pokemon);
+					pokemonDiv.classList.add(pokemonData[pokemon]);
+				}
+			}
+		}
+	} else {
+		return;
+	}
+}
+
+function setDexState (nationalNormal, altNormal, nationalShiny, altShiny) {
+	const dexes = [
+		{	state: 	nationalNormal,
+			id:		'normal-nat-dex' },
+		{	state: 	altNormal,
+			id:		'normal-alt-dex' },
+		{	state: 	nationalShiny,
+			id:		'shiny-nat-dex' },
+		{	state: 	altShiny,
+			id:		'shiny-alt-dex' }
+	];
+	for (dexIndex in dexes) {
+		const dex = dexes[dexIndex];
+		if (dex.state === false) {
+			const dexDiv = document.getElementById(dex.id);
+			const showHide = document.getElementById(dex.id.replace('dex', 'showhide'));
+			dexDiv.style.display = 'none';
+			showHide.firstChild.innerHTML = `<img src="../assets/icons/show.svg" alt="">&nbsp;Show&nbsp;`;
+		}
+	}
+}
+
+// Fetch the database's profile data for the user and display it
+function getProfileData (user) {
+	// Fetch the user's data
+	const userData = db.collection('userData').doc(user.uid);
+	
+	// Check to see if user has checklist progress in the database
+	userData.get().then((doc) => {
+		const data = doc.data();
+		if (data.pokemon) {
+			writeUserDex (data.pokemon, data.autoCollapse);
+		} else {
+			console.log ('Error getting user data: no data present');
+		}
+		if (data.dexDisplay) {
+			const nationalNormal = data.dexDisplay.setNationalNormal;
+			const altNormal = data.dexDisplay.setAltNormal;
+			const nationalShiny = data.dexDisplay.setNationalShiny;
+			const altShiny = data.dexDisplay.setAltShiny;
+			setDexState (nationalNormal, altNormal, nationalShiny, altShiny);
+		} else {
+			console.log ('Error getting user data: no data present');
+		}
+	}).catch((error) => {
+		console.log('Error getting user data: ', error);
+	});
+}
