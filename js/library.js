538fa8d91f37e5b024fc567dd00cc95f3969a169
sdcervi <sdcervi@gmail.com>
2023-12-10 12:16:57 -0800

    Progress on Library

--- js/library.js
+++ js/library.js
@@ -1,37 +1,81 @@
 const libraryDiv = document.getElementById('libraryContent');
 
-function writeProduct (brand, productID) {
-	let productContent;
-	productContent = `<p>${brand} ${productID}`;
+function writeProduct (brand, productID, productDetails) {
+	// Get the human-readable brand name from the brand ID
+	const brandName = brand.replace("_", " ").replace(
+		/\w\S*/g,
+		function(txt) {
+			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+		}
+	);
+	const productType = productDetails.type;
+	const productName = productDetails.name;
+	const productLaunch = productDetails.launchdate;
+	
+	// Set up the product image, with a default fail-over if the image doesn't exist
+	let productImage = "";
+	if (productType === "product") {
+		// If the product image can have variants such as skin tone and nail length
+		productImage = `/assets/products/${brand}/${productID}.jpg`;
+	} else {
+		// If the product image doesn't have any variants
+		productImage = `/assets/products/${brand}/${productID}.jpg`;
+	}
+	
+	let productContent = "";
+	
+	productContent += `<img src="${productImage}" onerror="this.src='/assets/comingsoon.png';" alt="" />`;
+	productContent += `<p>${brandName}</p>`;
+	productContent += `<p>${productName}</p>`;
+	
+	if (productType === "product") {
+		productContent += `<p>${productDetails.formula}</p>`;
+		productContent += `<p>Launched ${productDetails.launchdate}</p>`;
+		if (productDetails.retireDate && !productDetails.restocked) {
+			productContent += `<p>Retired ${productDetails.retireDate}</p>`;
+			productContent += `<p>Alternatives: `;
+			productDetails.alternatives.forEach(alternative => {
+				productContent += `<a href="${alternative[1]}">${alternative[0]}</a>`;
+			});
+			productContent += `</p>`;
+		} else {
+			productContent += `<p><a href="${productDetails.link}" class="btn btn-primary btn-sm">Shop now</a></p>`;
+		}
+		productContent += `<p>In collections: `;
+		console.log (productDetails.collections);
+		productDetails.collections.forEach(collection => {
+			productContent += `${library[brand][collection].name}`;
+		});
+		productContent += `</p>`;
+	}
+	
 	return productContent;
 }
 
-function writeLibrary (products) {
-	// products will come in as a pre-sorted array
+function writeLibrary () {
+	const productArray = [];
+	for (const brandIndex in library) {
+		const brand = library[brandIndex];
+		for (const productIndex in brand) {
+			const product = brand[productIndex];
+			productArray.push([brandIndex, productIndex, product]);
+		}
+	}
 	
-	// Get the HTML div
-	const libraryDiv = document.getElementById('libraryContent');
-	let libraryDivContents;
+	// Sort by: alphabetica A-Z, alphabetical Z-A, release date earliest, release date latest
+	
+	let libraryDivContents = "";
 	
 	// Generate the content
-	products.forEach(product => {
-		console.log (product);
+	productArray.forEach(product => {
+		const productHTML = writeProduct(product[0], product[1], product[2]);
+		libraryDivContents += productHTML;
 	});
+	
+	libraryDiv.innerHTML = libraryDivContents;
 }
 
-const productArray = [];
-for (const brandIndex in library) {
-	const brand = library[brandIndex];
-	console.log (brandIndex);
-	for (const productIndex in brand) {
-		const product = brand[productIndex];
-		productArray.push([brandIndex, productIndex, product]);
-	}
-}
-
-// Sort by: alphabetica A-Z, alphabetical Z-A, release date earliest, release date latest
-
-
+writeLibrary();
 
 
 
